a48affb3d836b3de2b92937954ad0775c6412b45
Othman Haba  (ง'̀-'́)ง <124089971+OthmanHaba@users.noreply.github.com>
2025-09-07 21:25:40 +0200

    seeder

---
+++ database/seeders/ShieldSeeder.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Database\Seeders;
+
+use BezhanSalleh\FilamentShield\Support\Utils;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\PermissionRegistrar;
+
+class ShieldSeeder extends Seeder
+{
+    public function run(): void
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $rolesWithPermissions = '[{"name":"super_admin","guard_name":"web","permissions":["view_activity::log","view_any_activity::log","create_activity::log","update_activity::log","view_currency","view_any_currency","create_currency","update_currency","view_customer","view_any_customer","create_customer","update_customer","view_invoice","view_any_invoice","create_invoice","update_invoice","view_item","view_any_item","create_item","update_item","view_receipt","view_any_receipt","create_receipt","update_receipt","view_role","view_any_role","create_role","update_role","delete_role","delete_any_role","view_treasure","view_any_treasure","create_treasure","update_treasure","view_user","view_any_user","create_user","update_user","page_FinancialReport","page_ReportsPage","page_Setting","widget_StatsOverviewWidget","widget_QuickActionsWidget","widget_RevenueChartWidget","widget_ReceiptsChartWidget","widget_RecentInvoicesWidget","widget_TreasuryBalanceWidget"]}]';
+        $directPermissions = '[]';
+
+        static::makeRolesWithPermissions($rolesWithPermissions);
+        static::makeDirectPermissions($directPermissions);
+
+        $this->command->info('Shield Seeding Completed.');
+    }
+
+    protected static function makeRolesWithPermissions(string $rolesWithPermissions): void
+    {
+        if (! blank($rolePlusPermissions = json_decode($rolesWithPermissions, true))) {
+            /** @var Model $roleModel */
+            $roleModel = Utils::getRoleModel();
+            /** @var Model $permissionModel */
+            $permissionModel = Utils::getPermissionModel();
+
+            foreach ($rolePlusPermissions as $rolePlusPermission) {
+                $role = $roleModel::firstOrCreate([
+                    'name' => $rolePlusPermission['name'],
+                    'guard_name' => $rolePlusPermission['guard_name'],
+                ]);
+
+                if (! blank($rolePlusPermission['permissions'])) {
+                    $permissionModels = collect($rolePlusPermission['permissions'])
+                        ->map(fn ($permission) => $permissionModel::firstOrCreate([
+                            'name' => $permission,
+                            'guard_name' => $rolePlusPermission['guard_name'],
+                        ]))
+                        ->all();
+
+                    $role->syncPermissions($permissionModels);
+                }
+            }
+        }
+    }
+
+    public static function makeDirectPermissions(string $directPermissions): void
+    {
+        if (! blank($permissions = json_decode($directPermissions, true))) {
+            /** @var Model $permissionModel */
+            $permissionModel = Utils::getPermissionModel();
+
+            foreach ($permissions as $permission) {
+                if ($permissionModel::whereName($permission)->doesntExist()) {
+                    $permissionModel::create([
+                        'name' => $permission['name'],
+                        'guard_name' => $permission['guard_name'],
+                    ]);
+                }
+            }
+        }
+    }
+}
